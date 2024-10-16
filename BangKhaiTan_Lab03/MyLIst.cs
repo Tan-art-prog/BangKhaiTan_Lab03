6901da759e5d8555726ae02e735eac8d41de1c03
SV <SV@PM20-PC32>
2024-10-16 08:29:39 +0700

    Add project files.

---
+++ BangKhaiTan_Lab03/MyLIst.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangKhaiTan_Lab03
+{
+    internal class MyList
+    {
+        private IntNode first;
+        private IntNode last;
+
+        public IntNode First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+        public IntNode Last
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        public MyList()
+        {
+            first = null;
+            last = null;
+        }
+        public bool IsEmpty()
+        {
+            return first == null;
+        }
+        public void AddFirst(IntNode newNode)
+        {
+            if (IsEmpty())
+                first = last = newNode;
+            else
+            {
+                newNode.Next = first;
+                first = newNode;
+            }
+        }
+
+
+
+        public void Input()
+        {
+            int x;
+            do
+            {
+                Console.Write("Gia tri (0 ket thuc): ");
+                int.TryParse(Console.ReadLine(), out x);
+                if (x == 0)
+                    return;
+                IntNode newNode = new IntNode(x);
+                AddFirst(newNode);
+            } while (true);
+        }
+        public void ShowList()
+        {
+            IntNode p = first;
+            while (p != null)
+            {
+                Console.Write("{0} -> ", p.Data);
+                p = p.Next;
+            }
+            Console.WriteLine("null");
+        }
+
+        public IntNode SearchX(int x)
+        {
+            IntNode current = first;
+            while (current != null)
+            {
+                if (current.Data == x)
+                    return current;
+                current = current.Next;
+            }
+            return null; // Không tìm thấy
+        }
+
+
+        public IntNode GetMax()
+        {
+            if (IsEmpty())
+                return null;
+
+            IntNode current = first;
+            IntNode maxNode = first;
+            while (current == null)
+            {
+                if (current.Data > maxNode.Data)
+                    maxNode = current;
+                current = current.Next;
+            }
+            return maxNode;
+        }
+
+        public IntNode GetMin()
+        {
+            if (IsEmpty()) return null;
+
+            IntNode current = first;
+            IntNode minNode = first;
+            while (current != null)
+            {
+                if (current.Data < minNode.Data)
+                    minNode = current;
+                current = current.Next;
+            }
+            return minNode;
+        }
+
+        public MyList GetEvenList()
+        {
+            MyList eventList = new MyList();
+            IntNode p = first;
+            while (p != null)
+            {
+                if ((p.Data & 2) != 0)
+                {
+                    eventList.AddLast(new IntNode(p.Data));
+                }
+                p = p.Next;
+            }
+            return eventList;
+        }
+
+        private void AddLast(IntNode intNode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public MyList GetOddList()
+        {
+            MyList oddList = new MyList();
+            IntNode p = first;
+            while (p != null)
+            {
+                if (p.Data % 2 != 0)
+                    oddList.AddLast(new IntNode(p.Data));
+                p = p.Next;
+            }
+            return oddList;
+        }
+    }
+}
---
+++ BangKhaiTan_Lab03/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BangKhaiTan_Lab03;
+
+MyList list = new MyList();
+list.Input();
+list.ShowList();
+Console.WriteLine("Nhap so can tim: ");
+int x = Convert.ToInt32(Console.ReadLine());
+IntNode kq = list.SearchX(x);
+if (kq == null)
+    Console.WriteLine("khong tim thay: ");
+else
+    Console.WriteLine("Tim thay: ");
+Console.WriteLine("Gia tri lon nhat la: " + list.GetMax());
